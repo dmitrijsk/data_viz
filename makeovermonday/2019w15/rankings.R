c0e883e166af52028fcf226d62984d99cfb6b7d4
me_home <dmitrijs.kass@gmail.com>
2019-04-08 02:37:18 +0300

    Before changing to deviation from overall ranking.

--- makeovermonday/2019w15/rankings.R
+++ makeovermonday/2019w15/rankings.R
@@ -20,11 +20,9 @@ coord_radar <- function (theta = "x", start = 0, direction = 1) {
 #                      "Below Average" = "grey90", # originally "#EC9247"
 #                      "Bottom Five" = "#E3643B")
 
-COLOURS_OVERALL <- c("Top Five" = "#00787E", 
-                     "Above Average" = "#1DA7AA",
-                     "Average" = "grey90", # originally "#91C6BE"
-                     "Below Average" = "grey90", # originally "#EC9247"
-                     "Bottom Five" = "#E3643B")
+COLOURS_OVERALL <- c("Top Ten" = "#1DA7AA", 
+                     "Average" = "grey90",
+                     "Bottom Ten" = "#EC9247")
 
 # Get data.
 GET("https://query.data.world/s/43zkfzcdmvcgmp6bl3ju5kgwmxhnv6", write_disk(tf <- tempfile(fileext = ".xlsx")))
@@ -54,11 +52,9 @@ rankings_with_mad <- rankings %>%
 rankings_with_mad %>% 
   # filter(overallrank %in% c(1:5, 46:50)) %>% 
   mutate(State = reorder(State, -mean_abs_dev)) %>% 
-  ggplot(aes(x = State, y = mean_abs_dev, fill = case_when(overallrank %in% 1:5 ~ "Top Five",
-                                                    overallrank %in% 6:17 ~ "Above Average",
-                                                    overallrank %in% 18:32 ~ "Average",
-                                                    overallrank %in% 33:45 ~ "Below Average",
-                                                    TRUE ~ "Bottom Five"))) +
+  ggplot(aes(x = State, y = mean_abs_dev, fill = case_when(overallrank %in% 1:10 ~ "Top Ten",
+                                                    overallrank %in% 41:50 ~ "Bottom Ten",
+                                                    TRUE ~ "Average"))) +
   geom_col() +
   scale_y_continuous(breaks = seq(0, 20, by = 10), expand = c(0, 0, 0, 20)) +
   scale_fill_manual(limits = names(COLOURS_OVERALL), values = COLOURS_OVERALL) +
@@ -73,16 +69,14 @@ rankings_with_mad %>%
   annotate(y = 25, x = 48, geom = "text", label = "Uniform fiscal condition", hjust = 0, size = 7, colour = "grey30") +
   annotate(y = 25, x = 3, geom = "text", label = "Volatile fiscal condition", hjust = 0, size = 7, colour = "grey30")
 
-ggsave(filename = "bars_five_dimensions.png", height = 30, width = 25, units = "cm", dpi = 150)
+ggsave(filename = "bars_dimensions_3_groups.png", height = 30, width = 25, units = "cm", dpi = 150)
 
 # Radar plot.
 plot_radar <- function(group) {
   
-  overallrating_vec <- case_when(group == "Top Five" ~ list(1:5),
-                                 group == "Above Average" ~ list(6:17),
-                                 group == "Average" ~ list(18:32),
-                                 group == "Below Average" ~ list(33:45),
-                                 TRUE ~ list(46:50))[[1]]
+  overallrating_vec <- case_when(group == "Top Ten" ~ list(1:10),
+                                 group == "Bottom Ten" ~ list(41:50),
+                                 TRUE ~ list(11:40))[[1]]
     
   rankings %>% 
     filter(overallrank %in% overallrating_vec) %>% 
@@ -102,7 +96,8 @@ plot_radar <- function(group) {
           panel.grid.major.x = element_line(linetype = "dashed"),
           strip.background = element_rect(fill = "white"), 
           panel.border = element_blank(),
-          panel.spacing.x = unit(2,"line"),
+          panel.spacing.x = unit(2,"line"), 
+          panel.spacing.y = unit(2,"line"), 
           strip.text = element_text(face = "bold", size = 12, color = "grey30")) +
     labs(title = "Five dimensions of solvency",
          subtitle = "Trust fund (T), Cash (C), Long-run (L), Budget (B), Service-level (S), \n",
@@ -110,14 +105,11 @@ plot_radar <- function(group) {
          y = NULL)
 }
 
-plot_radar("Top Five")
-ggsave(filename = "radar_top_five.png", height = 8, width = 25, units = "cm", dpi = 150)
+plot_radar("Top Ten")
+ggsave(filename = "radar_top_ten.png", height = 12, width = 25, units = "cm", dpi = 150)
 
-plot_radar("Above Average")
-ggsave(filename = "radar_above_average.png", height = 8, width = 25, units = "cm", dpi = 150)
-
-plot_radar("Bottom Five")
-ggsave(filename = "radar_bottom_five.png", height = 8, width = 25, units = "cm", dpi = 150)
+plot_radar("Bottom Ten")
+ggsave(filename = "radar_bottom_ten.png", height = 12, width = 25, units = "cm", dpi = 150)
 
 
 
